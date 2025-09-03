b93d35ab9018e4c61e165408975e1ad85bad43d9
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-09-03 11:28:19 +0530

    first commit

---
+++ ObjArr.java
@@ -0,0 +1,13 @@
+package collection.gth;
+public class ObjArr 
+{   public static void main(String args[])
+	{    Object a[]=new Object[5];
+	     a[0]=1;
+	     a[1]="Rahul";
+	     a[2]=4.3f;
+	     a[3]=false;
+    for(int i=0;i<a.length;i++)
+    {   System.out.println(a[i]);
+    }
+  }
+}

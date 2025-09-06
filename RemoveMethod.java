29a5fc9dad8720e0603679bc1ffe96b113b91786
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-09-06 16:13:45 +0530

    Add files via upload
    Remove element from specific index for use this method

---
+++ RemoveMethod.java
@@ -0,0 +1,20 @@
+package collection.gth;
+
+import java.util.ArrayList;
+
+public class RemoveMethod {
+	public static void main(String[] args) 
+	{
+		ArrayList al=new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		
+		  al.remove(1);
+		System.out.println(al);
+
+	}
+
+}

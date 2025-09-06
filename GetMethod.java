567b1a3409fb96e79e7fbcf6df897aa68e653f45
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-09-06 16:00:19 +0530

    Add files via upload
    In this application we use get method for check any index on element present .

---
+++ GetMethod.java
@@ -0,0 +1,18 @@
+package collection.gth;
+
+import java.util.ArrayList;
+
+public class GetMethod {
+	public static void main(String[] args) 
+	{   ArrayList al=new ArrayList();
+	   al.add(10);
+	   al.add(20);
+	   al.add(30);
+	   al.add(40);
+	   al.add(50);
+	   
+	   System.out.println("Get Method :"+al.get(2));
+	
+	}
+
+}

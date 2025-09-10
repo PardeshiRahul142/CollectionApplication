79a7c154a1ace5f3d681f4383ee879e51fe74513
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-09-10 10:03:57 +0530

    Add files via upload
    In this code we implements clear() method for remove all element from collecton

---
+++ ClearMethod.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ClearMethod
+{   public static void main(String args[])
+    {
+       ArrayList al=new ArrayList();
+       al.add(10);
+       al.add(20);
+       al.add(30);
+       al.add(40);
+       al.add(50);
+
+       al.clear();
+     System.out.println(al);
+
+    }
+
+}

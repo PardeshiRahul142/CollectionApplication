4b21301bdd717344679f285bc957df2b0d482ca3
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-09-15 12:32:23 +0530

    Add files via upload

---
+++ VectorCity.java
@@ -0,0 +1,20 @@
+// Write the program Vector of city
+import java.util.*;
+public class VectorCity
+{   public static void main(String args[])
+    {
+      Vector v=new Vector();
+      v.add("Pune");
+      v.add("Mumbai");
+      v.add("Hydrabad");
+      v.add("Delhi");
+      v.add("Banglor");
+  
+     v.remove(3);
+     v.set(1,"Karjat");
+     
+    for(int i=0;i<v.size();i++)
+    {  System.out.println(obj);
+    }
+    }//main
+}//class

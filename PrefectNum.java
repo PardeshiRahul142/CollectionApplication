e585917512a64b4e778c161f3737451d50d4bd75
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-10-08 16:44:33 +0530

    Add files via upload

---
+++ PrefectNum.java
@@ -0,0 +1,21 @@
+//perfect numbet
+import java.util.*;
+public class PrefectNum
+{   public static void main(String args[])
+    {
+       int n=100;
+       for(int i=1;i<=n;i++)
+       {
+         int sum=0;
+         for(int j=1;j<=i/2;j++)
+         {
+            if(i%j==0)
+            { sum=sum+j;
+            }
+         }
+        if(sum == i)
+          { System.out.println(i);
+          }
+       }
+    }//main
+}//class

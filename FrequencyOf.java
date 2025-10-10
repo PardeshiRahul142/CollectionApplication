c2ac2ad261dd20d0151015c0a4abcc8cbce3cc00
Pardeshi Rahul <rahulpardeshi949@gmail.com>
2025-10-10 15:24:40 +0530

    Add files via upload

---
+++ FrequencyOf.java
@@ -0,0 +1,29 @@
+/*3.Find Frequency of Each Element in ArrayList
+Use a HashMap<T, Integer> to count occurrences of each element.*/
+
+import java.util.*;
+public class FrequencyOf
+{  public static void main(String args[])
+   {
+     int a[]={10,20,30,40,10,20,30,40,50};
+
+     HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+     for(int val : a)
+     {  Integer count=map.get(val);
+       
+        if(count==null)
+        {  map.put(val,1);
+        }
+       else
+        { map.put(val,count+1);
+        }
+     }
+     Set<Map.Entry<Integer,Integer>> set=map.entrySet();
+     for(Map.Entry<Integer,Integer>m:set)
+     { if(m.getValue() > 0)
+       { System.out.println(m.getKey()+"----->"+m.getValue());
+       }
+     }
+
+   }
+}//class
